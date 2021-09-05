0fabb61ab561d9a8fd3209be3fd21e1782602530
Zak Ali <zak.ali@thetin.local>
2021-09-05 20:07:15 +0100

    Fix styling flicker issue

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class Doc extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html>
