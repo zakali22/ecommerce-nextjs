2ef8ec41f6051a731b380e9b752f1a10c92c1305
Zak Ali <zak.ali@thetin.local>
2021-09-06 22:33:10 +0100

    Add authentication to keystone cms

--- backend/.keystone/admin/pages/_app.js
+++ backend/.keystone/admin/pages/_app.js
@@ -7,10 +7,11 @@ import { Core } from '@keystone-ui/core';
 import * as viewcd7b6d98 from '../../../node_modules/@keystone-next/fields/types/mongoId/views';
 import * as viewb3590f09 from '../../../node_modules/@keystone-next/fields/types/text/views';
 import * as view6d20965d from '../../../node_modules/@keystone-next/fields/types/password/views';
+import * as viewdd2f243a from '../../../node_modules/@keystone-next/fields/types/timestamp/views';
 
 const adminConfig = {};
 
-const fieldViews = { viewcd7b6d98, viewb3590f09, view6d20965d };
+const fieldViews = { viewcd7b6d98, viewb3590f09, view6d20965d, viewdd2f243a };
 
 const lazyMetadataQuery = {
   kind: 'Document',
@@ -150,6 +151,29 @@ const lazyMetadataQuery = {
             },
             loc: { start: 22, end: 234 },
           },
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'authenticatedItem' },
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {
+                  kind: 'InlineFragment',
+                  typeCondition: {
+                    kind: 'NamedType',
+                    name: { kind: 'Name', value: 'User' },
+                  },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
         ],
       },
     },
@@ -161,7 +185,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="1vkjrnd"
+        adminMetaHash="thggsw"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 
 const databaseUrl = process.env.DATABASE_URL;
@@ -8,25 +14,41 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+// Define auth settings
+const { withAuth } = createAuth({
+  listKey: 'User', // The administrator schema
+  identityField: 'email', // How to identify the User
+  secretField: 'password',
+  initFirstItem: {
+    // When no User exists, request registration
+    fields: ['name', 'email', 'password'],
+    // Define the roles for the User
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseUrl,
-  },
-  lists: createSchema({
-    // List of schema goes here
-    User,
-  }),
-  ui: {
-    // Access to the Keystone dashboard for DB management
-    // Change based on roles
-    isAccessAllowed: () => true,
-  },
-  // Add session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseUrl,
+    },
+    lists: createSchema({
+      // List of schema goes here
+      User,
+    }),
+    ui: {
+      // Access to the Keystone dashboard for DB management
+      // Only gives access to the User with a session
+      isAccessAllowed: ({ session }) => !!session?.data, // Return true if session exists (!! to convert into boolean)
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email', // From each session get the id, name, email
+    }),
+  })
+);
