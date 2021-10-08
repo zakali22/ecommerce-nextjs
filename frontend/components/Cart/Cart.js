6453ed1057c125e3dc061d9ead3ff4160a50f694
Zak Ali <zak.ali@thetin.local>
2021-10-08 15:38:46 +0100

    Add cart listing

--- frontend/components/Cart/Cart.js
+++ frontend/components/Cart/Cart.js
@@ -3,25 +3,34 @@ import CartStyles from '../styles/CartStyles';
 import Supreme from '../styles/Supreme';
 import useCurrentUser from '../../lib/useCurrentUser';
 import CartItem from './CartItem';
+import totalCartCalc from '../../lib/totalCartCalc';
+import formatPrice from '../../lib/formatPrice';
 
 export default function Cart() {
   const user = useCurrentUser();
 
   if (!user) return null;
-  console.log(user.cart);
+
+  // Total in cart
+  // Product
+  // total Price = quantity * price each
 
   return (
     <CartStyles open>
       <header>
         <Supreme>{user.name}'s cart</Supreme>
+        <p>
+          You have {user.cart.length} item{user.cart.length > 1 ? 's' : ''} in
+          your cart
+        </p>
       </header>
       <ul>
-        {/* {user.cart.map((cartItem) => (
+        {user.cart.map((cartItem) => (
           <CartItem key={cartItem.id} cartItem={cartItem} />
-        ))} */}
+        ))}
       </ul>
       <footer>
-        <p>Footer</p>
+        <p>Total: {formatPrice(totalCartCalc(user.cart))}</p>
       </footer>
     </CartStyles>
   );
--- frontend/components/Cart/CartItem.js
+++ frontend/components/Cart/CartItem.js
@@ -1,3 +1,50 @@
-export default function CartItem(cartItem) {
-  return <p>{cartItem.name}</p>;
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import formatPrice from '../../lib/formatPrice';
+
+const CartItemStyle = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  padding-bottom: 20px;
+  padding-right: 30px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+  img {
+    max-width: 120px;
+    height: auto;
+    object-fit: contain;
+    margin-right: 20px;
+  }
+
+  .cart-item-desc {
+    margin-right: auto;
+  }
+`;
+
+export default function CartItem({ cartItem }) {
+  console.log(cartItem);
+
+  if (!cartItem.product) return null;
+
+  return (
+    <CartItemStyle>
+      <img
+        src={cartItem.product.photo.image.publicUrlTransformed}
+        alt={cartItem.product.photo.image.altText}
+      />
+      <div className="cart-item-desc">
+        <p>{cartItem?.product?.name}</p>
+        <p>
+          {formatPrice(parseInt(cartItem.quantity) * cartItem.product.price)} -{' '}
+          {cartItem.quantity} &times; {formatPrice(cartItem.product.price)}
+        </p>
+      </div>
+      <span>&times;</span>
+    </CartItemStyle>
+  );
 }
+
+CartItem.propTypes = {
+  cartItem: PropTypes.object,
+};
